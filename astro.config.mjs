f72ef44436522821861d58ac32133014c8056d7a
Neroot <nerooot@tutamail.com>
2026-01-10 20:22:15 -0600

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://neroot.netlify.app/",
+  site: "https://neroot.vercel.app/",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "neroot",
-  EMAIL: "hifrommail@proton.me",
+  EMAIL: "nerooot@tutamail.com",
   NUM_POSTS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 2,
 };
